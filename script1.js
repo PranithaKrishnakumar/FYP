2b4164ec9621de26973e0ed4a49524854c9305ef
PranithaK97 <krishnakumarpranitha@gmail.com>
2023-05-04 13:45:17 +0530

    files to upload

---
+++ script1.js
@@ -0,0 +1,123 @@
+const modalWrapper = document.querySelector('.modal-wrapper');
+// modal add
+const addModal = document.querySelector('.add-modal');
+const addModalForm = document.querySelector('.add-modal .form');
+
+// modal edit
+const editModal = document.querySelector('.edit-modal');
+const editModalForm = document.querySelector('.edit-modal .form');
+
+const btnAdd = document.querySelector('.btn-add');
+
+const tableUsers = document.querySelector('.table-users');
+
+let id;
+
+// Create element and render users
+const renderUser = doc => {
+  const tr = `
+    <tr data-id='${doc.id}'>
+      <td>${doc.data().receiverMailAddress}</td>
+      <td>${doc.data().senderMailAddress}</td>
+      <td>${doc.data().placeType}</td>
+
+      <td>
+        <button class="btn btn-edit">Edit</button>
+        <button class="btn btn-delete">Delete</button>
+      </td>
+    </tr>
+  `;
+  tableUsers.insertAdjacentHTML('beforeend', tr);
+
+  // Click edit user
+  const btnEdit = document.querySelector(`[data-id='${doc.id}'] .btn-edit`);
+  btnEdit.addEventListener('click', () => {
+    editModal.classList.add('modal-show');
+
+    id = doc.id;
+    editModalForm.receiverMailAddress.value = doc.data().receiverMailAddress;
+    editModalForm.senderMailAddress.value = doc.data().senderMailAddress;
+    editModalForm.placeType.value = doc.data().placeType;
+  });
+
+  // Click delete user
+  const btnDelete = document.querySelector(`[data-id='${doc.id}'] .btn-delete`);
+  btnDelete.addEventListener('click', () => {
+    db.collection('letters').doc(`${doc.id}`).delete().then(() => {
+      console.log('Document succesfully deleted!');
+    }).catch(err => {
+      console.log('Error removing document', err);
+    });
+  });
+
+}
+
+// Click add user button
+btnAdd.addEventListener('click', () => {
+  addModal.classList.add('modal-show');
+
+  addModalForm.receiverMailAddress.value = '';
+  addModalForm.senderMailAddress.value = '';
+  addModalForm.placeType.value = '';
+
+});
+
+// User click anyware outside the modal
+window.addEventListener('click', e => {
+  if(e.target === addModal) {
+    addModal.classList.remove('modal-show');
+  }
+  if(e.target === editModal) {
+    editModal.classList.remove('modal-show');
+  }
+});
+
+// Get all users
+// db.collection('users').get().then(querySnapshot => {
+//   querySnapshot.forEach(doc => {
+//     renderUser(doc);
+//   })
+// });
+
+// Real time listener
+db.collection('letters').onSnapshot(snapshot => {
+  snapshot.docChanges().forEach(change => {
+    if(change.type === 'added') {
+      renderUser(change.doc);
+    }
+    if(change.type === 'removed') {
+      let tr = document.querySelector(`[data-id='${change.doc.id}']`);
+      let tbody = tr.parentElement;
+      tableUsers.removeChild(tbody);
+    }
+    if(change.type === 'modified') {
+      let tr = document.querySelector(`[data-id='${change.doc.id}']`);
+      let tbody = tr.parentElement;
+      tableUsers.removeChild(tbody);
+      renderUser(change.doc);
+    }
+  })
+})
+
+// Click submit in add modal
+addModalForm.addEventListener('submit', e => {
+  e.preventDefault();
+  db.collection('letters').add({
+    receiverMailAddress: addModalForm.receiverMailAddress.value,
+    senderMailAddress: addModalForm.senderMailAddress.value,
+    placeType: addModalForm.placeType.value,
+  });
+  modalWrapper.classList.remove('modal-show');
+});
+
+// Click submit in edit modal
+editModalForm.addEventListener('submit', e => {
+  e.preventDefault();
+  db.collection('letters').doc(id).update({
+    receiverMailAddress: editModalForm.receiverMailAddress.value,
+    senderMailAddress: editModalForm.senderMailAddress.value,
+    placeType: editModalForm.placeType.value,
+  });
+  editModal.classList.remove('modal-show');
+  
+});
